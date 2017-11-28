9f80adb62b0c02b4a32b0998ababa802586fb453
Kyle Parisi <kyleparisi@gmail.com>
2017-11-28 14:32:08 -0500

    Need newer version of ansible than apt has

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-apt update
+apt-get install software-properties-common -y
+apt-add-repository ppa:ansible/ansible -y
+apt-get update
 apt install python ansible -y
 ansible-playbook playbook.yml
